bf2cfeb5774d99e391b2466963f0b060abe750f3
OscarUmhey98 <70187709+OscarUmhey98@users.noreply.github.com>
2020-10-16 21:18:33 -0600

    Add files via upload

---
+++ Homework 9/Oscar's Portrait.js
@@ -0,0 +1,18 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(100,340,100);
+    ellipse(150,120,20,40)
+    ellipse(250,120,20,40)
+    triangle(180,90,200,60,220,90)
+    rect(130,150,30,10)
+    rect(240,150,30,10)
+    ellipse(200,200,70,30)
+    point(150,120)
+    point(250,120)
+    line(120,100,160,90)
+    line(280,100,240,90)
+    text('Oscar the Destroyer',150,300)
+  }
